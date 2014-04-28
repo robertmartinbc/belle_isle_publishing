1131bd3cf02f3d712b5873558eb8ea1df2af8670
Robert Martin <robertmartinbc@shaw.ca>
2014-04-27 19:47:09 -0700

    Created welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BelleIslePublishing::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/about"
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
